bde931e4261b75ccc90323b18599ddb42d33aaaa
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-27 01:50:56 +0700

    configuration properties

--- src/main/java/programmerzamannow/spring/validation/BelajarSpringValidationApplication.java
+++ src/main/java/programmerzamannow/spring/validation/BelajarSpringValidationApplication.java
@@ -2,7 +2,12 @@ package programmerzamannow.spring.validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import programmerzamannow.spring.validation.properties.DatabaseProperties;
 
+@EnableConfigurationProperties({
+		DatabaseProperties.class
+})
 @SpringBootApplication
 public class BelajarSpringValidationApplication {
 
